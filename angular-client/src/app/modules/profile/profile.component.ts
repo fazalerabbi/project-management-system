4fa0c5d31697497f8b8f8b7d2e6f31ffe7aafc7a
fazalerabbi <fazal_raja815@hotmail.com>
2017-09-04 12:17:21 +0500

    Map profile forms (update profile and update password) with Component fields.

--- angular-client/src/app/modules/profile/profile.component.ts
+++ angular-client/src/app/modules/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  @ViewChild('profileForm') updateProfileForm: NgForm;
+  @ViewChild('updatePasswordForm') updatePasswordForm: NgForm;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onProfileUpdate() {
+    console.log(this.updateProfileForm);
+  }
+
+  onPasswordUpdate() {
+    console.log(this.updatePasswordForm);
   }
 
 }
--- angular-client/src/app/modules/profile/profile.module.ts
+++ angular-client/src/app/modules/profile/profile.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { ProfileComponent } from './profile.component';
 import { ProfileRoutingModule } from './profile.routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ ProfileComponent ],
-  imports: [ ProfileRoutingModule ]
+  imports: [
+    FormsModule,
+    ProfileRoutingModule
+  ]
 })
 
 export class ProfileModule {}
